b8f016c04b3397d96a1f5d59db81a7a9c350583f
Selena Hawamdeh <selenahawamdeh@gmail.com>
2023-11-15 15:29:46 -0700

    Solve final practice

--- final_practice.rb
+++ final_practice.rb
@@ -6,43 +6,83 @@
 
 # 1: Write a method named greeting that returns a string with a general greeting. 
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
+def greeting
+    "Hello, welcome to this program"
+end
+
+puts greeting1 = greeting
+puts greeting2 = greeting
+
+# What is the return value of your method? Hello, welcome to this program, twice
+# How many arguments did you pass your method? No arguments were passed
 
 
 # 2: Write a method named custom_greeting that returns a greeting WITH a specific name.
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+
+def greeting(name)
+    "Hello, #{name}!"
+end
+
+puts greeting("Selena")
+puts greeting("Sam")
+
+# What is the return value of your method? Hello, Selena! and Hello, Sam! each in a new line
+# How many arguments did you pass your method? 2 arguments are passed
+# What data type was your argument(s)? strings
 
 
 # 3: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+def greet_person(first_name, middle_name, last_name)
+    "Hello, #{first_name} #{middle_name} #{last_name}"
+end
+puts greet_person("Selena", "H", "Kayyali")
+
+# What is the return value of your method? Hello, Selena H Kayyali
+# How many arguments did you pass your method? Three arguments passed
+# What data type was your argument(s)? strings
 
 
 # 4: Write a method named square that takes in one integer, and returns the square of that integer.
 # Bonus: Print a sentence that interpolates the return value of your square method.
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+def square(number)
+    number * number
+end
+square_number = square(6)
+
+puts "The square of 6 is #{square_number}"
+
+# What is the return value of your method? The square of 6 is 36
+# How many arguments did you pass your method? 1 argument
+# What data type was your argument(s)? Integer
 
 
 # 5: Write a method named check_stock that satisfies the following interaction pattern:
 # Hint: You will only write one check_stock method that checks the quantity and then prints the corresponding statement.
 
-check_stock(4, "Coffee");
+
+def check_stock(quantity, product)
+    if quantity == 0
+        "#{product} - OUT of stock!"
+    elsif quantity <= 3 
+        "#{product} - running LOW"
+    elsif quantity > 3 
+        "#{product} is stocked"    
+    end
+end
+
+
+
+puts check_stock(4, "Coffee");
 # => "Coffee is stocked"
 
-check_stock(3, "Tortillas");
+puts check_stock(3, "Tortillas");
 # => "Tortillas - running LOW"
 
-check_stock(0, "Cheese");
+puts check_stock(0, "Cheese");
 # => "Cheese - OUT of stock!"
 
-check_stock(1, "Salsa");
+puts check_stock(1, "Salsa");
 # => "Salsa - running LOW"
